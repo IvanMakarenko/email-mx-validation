40813d92a5ec0fd5bbf68b520e03d16ea4986749
Ivan Makarenko <shketiam@gmail.com>
2019-11-07 12:53:00 +0200

    Create EmailMxRecordValidator.php

---
+++ EmailMxRecordValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace email-mx-validation;
+
+use Yii;
+use yii\validators\Validator;
+
+class EmailMxRecordValidator extends Validator
+{
+    public function validateAttribute($model, $attribute)
+    {
+        list($user, $domain) = explode('@', $model->{$attribute});
+        if ($domain && !checkdnsrr($domain, 'MX')) {
+            $this->addError($model, $attribute, Yii::t('yii', 'Check e-mail for correctness'));
+        }
+    }
+}
